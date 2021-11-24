49fa200437be30cb02d1a16ad5a2ab07414e495b
Richelle Lobo <lobo.r0018@gmail.com>
2021-11-24 17:11:28 +0800

    Add validations for artists

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,5 @@
 class Artist < ApplicationRecord
+	validates :name, :permalink, :bio, :formed_at, presence: true
 	mount_uploader :avatar_url, AvatarUploader
 	mount_uploader :cover_photo_url, AvatarUploader
 end
