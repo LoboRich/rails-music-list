b3feb0e554da3264ec0aa6d43cc575a5293d9c7e
Richelle Lobo <lobo.r0018@gmail.com>
2021-11-24 20:10:52 +0800

    Add length validation for album

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,6 @@
 class Album < ApplicationRecord
 	validates :name, :released, :released_at, :cover_art_url, :kind, :length, presence: true 
 	validates :name, uniqueness: true
+	validates :length, numericality: { greater_than: 0 }
 	mount_uploader :cover_art_url, AvatarUploader
 end
