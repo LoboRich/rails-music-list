04e7135c6f25e8c53181d09ac71d5d385e4b3e6f
Richelle Lobo <lobo.r0018@gmail.com>
2021-11-24 19:10:16 +0800

    Add validation uniqness for names

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,5 @@
 class Album < ApplicationRecord
 	validates :name, :released, :released_at, :cover_art_url, :kind, :length, presence: true 
+	validates :name, uniqueness: true
 	mount_uploader :cover_art_url, AvatarUploader
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,6 @@
 class Artist < ApplicationRecord
 	validates :name, :permalink, :bio, :formed_at, presence: true
+	validates :name, uniqueness: true
 	mount_uploader :avatar_url, AvatarUploader
 	mount_uploader :cover_photo_url, AvatarUploader
 end
